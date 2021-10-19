bdfc83c32f71067a56598ace926e3cfbadb08675
pyama2000 <takahiko-yamashita@retty.me>
2021-10-20 00:13:08 +0900

    API Gatewayのコードをapi-gatewayワークスペースに移動

--- api-gateway/src/main.rs
+++ api-gateway/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod internal;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = config::Config::new();
+
+    let server = internal::server::Server::new(config);
+    server.run().await;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-mod config;
-mod internal;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = config::Config::new();
-
-    let server = internal::server::Server::new(config);
-    server.run().await;
-
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
